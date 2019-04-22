5f49bcfc1e17f7102a58b92a5f4041bd75125aca
ShaoQiBNU <37075909+ShaoQiBNU@users.noreply.github.com>
2019-04-22 21:54:33 +0800

    Update main.py

--- code/main.py
+++ code/main.py
@@ -11,19 +11,26 @@ def main():
     ch = config.ConfigHandler("./config.ini")
     ch.load_config()
 
+
     ########### 读取参数 ##########
-    batch_size = int(ch.config["model"]["batch_size"])
+    train_batch_size = int(ch.config["model"]["train_batch_size"])
+    val_batch_size = int(ch.config["model"]["val_batch_size"])
     num_epochs = int(ch.config["model"]["num_epochs"])
     learning_rate = float(ch.config["model"]["learning_rate"])
     class_size = int(ch.config["model"]["class_size"])
     log_interval = int(ch.config["log"]["log_interval"])
 
+
     ########### 获取数据loader ##########
-    data_loader_train = dataset.MyDataset(batch_size).load_train_data()
-    data_loader_test = dataset.MyDataset(batch_size).load_test_data()
+    data_loader = dataset.MyDataset(train_batch_size, val_batch_size)
+
+    data_loader_train =data_loader.load_train_data()
+    data_loader_test = data_loader.load_test_data()
+
 
     ########### 训练和评价 ##########
     train.train_and_test(num_epochs, learning_rate, class_size, data_loader_train, data_loader_test, log_interval).train_epoch()
+
 
 if __name__ == "__main__":
     main()
